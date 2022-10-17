81571a5bcbd2c0a69293a2fa1994c6dcdf9bc02d
Llerraf Gnem <lleerraaff@gmail.com>
2022-10-16 22:54:39 -0500

    remove capture field

--- src/Trout/MoveGen.hs
+++ src/Trout/MoveGen.hs
@@ -19,17 +19,17 @@ data SpecialMove
     | Castle Bool -- kingside?
     | EnPassant Int -- en passant pawn squaree
     | Promotion Int -- promote piece
--- from, to, captures
-data Move = Move Int Int Bool SpecialMove
+-- from, to
+data Move = Move SpecialMove Int Int
 
 -- TODO incomplete
 pawnMoves :: Maybe Int -> Int -> Color -> Bitboard -> [Move]
 pawnMoves enPSq sq White block =
-    [Move sq (sq + 8) False p | frontOpen,    p <- promotes] ++
-    [Move sq (sq + 7) True  p | captureLeft,  p <- promotes] ++
-    [Move sq (sq + 9) True  p | captureRight, p <- promotes] ++
-    [Move sq (sq + 16) False Normal | doubleFrontOpen] ++ -- can't promote
-    [Move sq en True (EnPassant en) | en <- enPassant]
+    [Move p sq (sq + 8) | frontOpen,    p <- promotes] ++
+    [Move p sq (sq + 7) | captureLeft,  p <- promotes] ++
+    [Move p sq (sq + 9) | captureRight, p <- promotes] ++
+    [Move Normal sq (sq + 16) | doubleFrontOpen] ++ -- can't promote
+    [Move (EnPassant en) sq en | en <- enPassant]
   where
     enPassant = filter
         (\e -> abs (e - sq) == 1 && (rank2 .&. bit sq /= 0))
@@ -43,11 +43,11 @@ pawnMoves enPSq sq White block =
     captureRight = not $ unblocked (sq + 9)
     unblocked s = inBoard s && not (testBit block s)
 pawnMoves enPSq sq Black block =
-    [Move sq (sq - 8) False p | frontOpen,    p <- promotes] ++
-    [Move sq (sq - 9) False p | captureLeft,  p <- promotes] ++
-    [Move sq (sq - 7) True  p | captureRight, p <- promotes] ++
-    [Move sq (sq - 16) False Normal | doubleFrontOpen] ++
-    [Move sq en True (EnPassant en) | en <- enPassant]
+    [Move p sq (sq - 8) | frontOpen,    p <- promotes] ++
+    [Move p sq (sq - 9) | captureLeft,  p <- promotes] ++
+    [Move p sq (sq - 7) | captureRight, p <- promotes] ++
+    [Move Normal sq (sq - 16) | doubleFrontOpen] ++
+    [Move (EnPassant en) sq en | en <- enPassant]
   where
     enPassant = filter
         (\e -> abs (e - sq) == 1 && (rank2 .&. bit sq /= 0))
@@ -62,28 +62,28 @@ pawnMoves enPSq sq Black block =
     unblocked s = inBoard s && not (testBit block s)
 
 knightMoves :: Int -> Bitboard -> [Move]
-knightMoves sq block = (\m -> Move sq m (testBit block m) Normal)
+knightMoves sq _ = Move Normal sq
     <$> filter inBoard ((sq +) <$> possDiffs)
   where
     possDiffs = [-17, -15, -10, -6, 6, 10, 15, 17]
 
 bishopMoves :: Int -> Bitboard -> [Move]
-bishopMoves sq block = (\m -> Move sq m (testBit block m) Normal)
+bishopMoves sq block = Move Normal sq
     <$> toSqs (bishopMovesMagic sq block)
 
 rookMoves :: Int -> Bitboard -> [Move]
-rookMoves sq block = (\m -> Move sq m (testBit block m) Normal)
+rookMoves sq block = Move Normal sq
     <$> toSqs (rookMovesMagic sq block)
 
 queenMoves :: Int -> Bitboard -> [Move]
-queenMoves sq block = (\m -> Move sq m (testBit block m) Normal)
+queenMoves sq block = Move Normal sq
     <$> toSqs (bishopMovesMagic sq block .|. rookMovesMagic sq block)
 
 kingMoves :: Bool -> Bool -> Int -> Bitboard -> [Move]
-kingMoves castleK castleQ sq block =
-    [Move sq (sq + 2) False (Castle True ) | castleK] ++
-    [Move sq (sq - 2) False (Castle False) | castleQ] ++
-    ((\m -> Move sq m (testBit block m) Normal)
+kingMoves castleK castleQ sq _ =
+    [Move (Castle True) sq (sq + 2) | castleK] ++
+    [Move (Castle False) sq (sq - 2) | castleQ] ++
+    (Move Normal sq
         <$> filter inBoard ((sq +) <$> possDiffs))
   where
     possDiffs = [xyToSq x y | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0]
