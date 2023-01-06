98e18b770006be70c53933213ca1bd81605abc7a
Llerraf Gnem <lleerraaff@gmail.com>
2023-01-05 22:40:44 -0600

    reformat test

--- test/CountMoves.hs
+++ test/CountMoves.hs
@@ -7,6 +7,6 @@ import Trout.Game
 
 startMovesSpec :: Spec
 startMovesSpec = describe "allMoves" $
-    context "at starting position" $ 
+    context "at starting position" $
         it "should return the right amount of moves" $
             allMoves startingGame `shouldSatisfy` ((== 8 + 8 + 2 + 2) . length)
