1e4d67c3fbfab953bfe63126548dab1a6a47290e
Llerraf Gnem <lleerraaff@gmail.com>
2023-02-03 18:54:48 -0600

    partial fen

---
+++ src/Trout/Fen/Parse.hs
@@ -0,0 +1,56 @@
+module Trout.Fen.Parse where
+
+import Data.Bifunctor         (first)
+import Data.Bits              ((.|.))
+import Data.Char              (digitToInt, isDigit)
+import Data.Functor           ((<&>), ($>))
+import Text.Parsec            (char, oneOf)
+import Text.Parsec.Combinator (many1)
+import Text.Parsec.String     (Parser)
+import Trout.Bitboard         (fromSqs)
+import Trout.Game             (Pieces (Pieces), CanCastle)
+import Trout.Piece (Color(White, Black))
+import Control.Applicative ((<|>))
+
+parsePieces :: Parser Pieces
+parsePieces = fenRows
+    <&> \pcs ->
+        piecesWithoutAll
+            (genBitboard 'p' pcs)
+            (genBitboard 'n' pcs)
+            (genBitboard 'b' pcs)
+            (genBitboard 'r' pcs)
+            (genBitboard 'q' pcs)
+            (genBitboard 'k' pcs)
+  where
+    expandRow _ [] = []
+    expandRow i (x : xs)
+        | isDigit x = (i, x) : expandRow (i + digitToInt x) xs
+        | otherwise = (i, x) : expandRow i xs
+    fenRow :: Int -> Parser [(Int, Char)]
+    fenRow y = map (first (\x -> x + y * 8))
+        . expandRow 0
+        <$> many1 (oneOf "1234567pnbrqk")
+    fenRows = concat
+        <$> sequence
+            [ fenRow 7 <* char '/'
+            , fenRow 6 <* char '/'
+            , fenRow 5 <* char '/'
+            , fenRow 4 <* char '/'
+            , fenRow 3 <* char '/'
+            , fenRow 2 <* char '/'
+            , fenRow 1 <* char '/'
+            , fenRow 0
+            ]
+    genBitboard c pcs = fromSqs
+        [ sq
+        | sq <- [0..63]
+        , Just c == lookup sq pcs
+        ]
+    piecesWithoutAll p n b r q k = Pieces
+        p n b r q k
+        (p .|. n .|. b .|. r .|. q .|. k)
+
+parseSide :: Parser Color
+parseSide = char 'w' $> White
+    <|> char 'b' $> Black
