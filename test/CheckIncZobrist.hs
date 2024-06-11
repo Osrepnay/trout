8af9ce91246c7f1c1a0090014cbd0d7603b8a13a
Llerraf Gnem <lleerraaff@gmail.com>
2024-06-10 19:23:25 -0500

    fix tests and now they fail (repeat zobrist one a bunch of times)

--- test/CheckIncZobrist.hs
+++ test/CheckIncZobrist.hs
@@ -7,15 +7,16 @@ import Data.Hashable (hash)
 import Lens.Micro ((^.))
 import Test.Hspec (Spec, context, describe)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (chooseInt, forAll, listOf)
+import Test.QuickCheck (chooseInt, forAll, listOf, withMaxSuccess)
 import Trout.Game (allMoves, hgGame, hgHash, makeMove, mkHGame, startingGame)
 
 incZobristSpec :: Spec
 incZobristSpec = describe "makeMove" $
   context "for random series of moves" $
     prop "should have the correct hash" $
-      forAll (listOf (chooseInt (0, maxBound))) $
-        \nums -> doInts nums (mkHGame startingGame)
+      withMaxSuccess 1000 $
+        forAll (listOf (chooseInt (0, maxBound))) $
+          \nums -> doInts nums (mkHGame startingGame)
   where
     -- uses ints as moves
     doInts (m : ms) hgame = case moved of
