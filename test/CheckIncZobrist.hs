157292d817ac63c862707e4564d2bd3d0a0a274c
Llerraf Gnem <lleerraaff@gmail.com>
2024-06-13 19:24:54 -0500

    fix zobrist test, remove microlens dependency

--- test/CheckIncZobrist.hs
+++ test/CheckIncZobrist.hs
@@ -4,25 +4,24 @@ module CheckIncZobrist
 where
 
 import Data.Hashable (hash)
-import Lens.Micro ((^.))
 import Test.Hspec (Spec, context, describe)
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck (chooseInt, forAll, listOf, withMaxSuccess)
-import Trout.Game (allMoves, hgGame, hgHash, makeMove, mkHGame, startingGame)
+import Trout.Game (Game (..), allMoves, makeMove, startingGame)
 
 incZobristSpec :: Spec
 incZobristSpec = describe "makeMove" $
   context "for random series of moves" $
     prop "should have the correct hash" $
-      withMaxSuccess 1000 $
+      withMaxSuccess 10000 $
         forAll (listOf (chooseInt (0, maxBound))) $
-          \nums -> doInts nums (mkHGame startingGame)
+          \nums -> doInts nums startingGame
   where
     -- uses ints as moves
-    doInts (m : ms) hgame = case moved of
+    doInts (m : ms) game = case moved of
       Just hg -> doInts ms hg
-      Nothing -> doInts [] hgame
+      Nothing -> doInts [] game
       where
-        moved = makeMove hgame (moves !! (m `rem` length moves))
-        moves = allMoves (hgame ^. hgGame)
-    doInts [] hgame = hgame ^. hgHash == hash (hgame ^. hgGame)
+        moved = makeMove game (moves !! (m `rem` length moves))
+        moves = allMoves game
+    doInts [] game = gameHash game == hash game
