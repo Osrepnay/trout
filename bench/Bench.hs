4d194a7bf89501e6b6bedad24021972423665c04
Llerraf Gnem <lleerraaff@gmail.com>
2025-06-22 14:40:34 -0500

    fix bench

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,20 +3,20 @@ import Criterion.Main
 import Data.Maybe
 import Trout.Game
 import Trout.Search
-import Trout.Search.TranspositionTable (HashMapTT)
+import Trout.Search.TranspositionTable (SizedHashMapTT)
 import Trout.Search.TranspositionTable qualified as TT
 
 main :: IO ()
 main = do
   -- probably better way to do this (env), cba read docs
-  let table = TT.new
+  let table = TT.sizedHMEmpty 1000000
   defaultMain
     [ bench "perft(5)" $ whnf (perft 5) startingGame,
       bench "bestMove depth 5" $
         whnf (bestMoveWrapper 5 startingGame) table
     ]
 
-bestMoveWrapper :: Int -> Game -> HashMapTT -> Int
+bestMoveWrapper :: Int -> Game -> SizedHashMapTT -> Int
 bestMoveWrapper depth game = fst . evalState (bestMove depth game)
 
 perft :: Int -> Game -> Int
