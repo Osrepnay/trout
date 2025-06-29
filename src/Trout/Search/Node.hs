0c76c3699c0cf603d032e8ecf699166fefe57678
Llerraf Gnem <lleerraaff@gmail.com>
2025-06-29 12:04:28 -0500

    fix ibv and use normal enum instead of ghc sh

--- src/Trout/Search/Node.hs
+++ src/Trout/Search/Node.hs
@@ -1,20 +1,13 @@
-{-# LANGUAGE MagicHash #-}
-
 module Trout.Search.Node (NodeType (..), NodeResult (..)) where
 
 import Foreign (Ptr, Storable (..), castPtr)
-import GHC.Base (Int (I#), dataToTag#, tagToEnum#)
 import Data.Bits ((.&.), complement)
 
 data NodeType
   = AllNode -- failed low, move is "too bad"
   | ExactNode -- not fail high or fail low
   | CutNode -- failed high/beta cutoff, move is "too good"
-  deriving (Eq, Show)
-
-instance Enum NodeType where
-  toEnum (I# i) = tagToEnum# i
-  fromEnum x = I# (dataToTag# x)
+  deriving (Enum, Eq, Show)
 
 data NodeResult = NodeResult
   { nodeResScore :: !Int,
@@ -28,10 +21,10 @@ toIbv (NodeResult score nodeType) =
   4 * score + fromEnum nodeType - 1
 
 fromIbv :: Int -> NodeResult
-fromIbv ibv = NodeResult rawScore (toEnum (diff + 1))
+fromIbv ibv = NodeResult (rounded `quot` 4) (toEnum (diff + 1))
   where
-    rawScore = ((ibv + 1) .&. complement 3) `quot` 4
-    diff = ibv - rawScore
+    rounded = (ibv + 1) .&. complement 3
+    diff = ibv - rounded
 
 instance Storable NodeResult where
   sizeOf :: NodeResult -> Int
